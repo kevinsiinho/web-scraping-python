508a840dfba72797ea2bcdf45dfc1b336d14e7d7
Kevin Andrades <83100672+kevinsiinho@users.noreply.github.com>
2023-07-26 17:50:50 -0500

    Delete libreria directory

--- libreria/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Link
-from .models import Frase
-from .models import Tipo
-
-# Register your models here.
-admin.site.register(Link)
-admin.site.register(Frase)
-admin.site.register(Tipo)
--- libreria/form.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Frase
-
-class FraseForm(forms.ModelForm):
-    class Meta:
-        model=Frase
-        fields = '__all__'
